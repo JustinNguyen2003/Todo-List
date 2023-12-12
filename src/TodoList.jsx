55e03709214059668a01fa36182b40496e33a223
JustinNguyen2003 <justinator999@gmail.com>
2023-12-11 21:45:14 -0800

    Code finished and working. Need to add comments.

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,12 +1,13 @@
 import { TodoItem } from "./TodoItem"
 
-export function TodoList({ todos }){
+export function TodoList({ todos, toggleTodo, deleteTodo }){
     return(
     <ul className="list">
       {todos.length === 0 && "All tasks completed"}
       {todos.map(todo => {
         return (
-            <TodoItem completed={todo.completed} id={todo.id} title={todo.title} key={todo.key} />
+            <TodoItem completed={todo.completed} id={todo.id} title={todo.title} key={todo.id}
+            toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
         )
       } )}
       
